a1bc6aea438f70dbcf963cb655fe7f978697c5df
ilzam <ilzammulkhaq85@gmail.com>
2024-03-31 22:32:25 +0700

    feat: get all ticket

--- lib/presentation/pages/ticket/list/ticket_list_page.dart
+++ lib/presentation/pages/ticket/list/ticket_list_page.dart
@@ -1,6 +1,10 @@
 import 'package:appflowy_board/appflowy_board.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:next_starter/data/models/ticket/ticket_model.dart';
+import 'package:next_starter/injection.dart';
+import 'package:next_starter/presentation/pages/ticket/list/cubit/ticket_list_cubit.dart';
 import 'package:next_starter/presentation/pages/ticket/list/widgets/text_item.dart';
 import 'package:next_starter/presentation/pages/ticket/list/widgets/ticket_card.dart';
 import 'package:next_starter/presentation/routes/app_router.dart';
@@ -20,12 +24,14 @@ class _TicketListPageState extends State<TicketListPage> {
   final AppFlowyBoardController controller = AppFlowyBoardController();
   final AppFlowyBoardScrollController boardScroll = AppFlowyBoardScrollController();
 
+  final bloc = locator<TicketListCubit>();
+
   DateTime now = DateTime.now();
   var start = DateTime(DateTime.now().year, DateTime.now().month, 1);
   var end = DateTime(DateTime.now().year, DateTime.now().month + 1, 0);
 
-  final config = AppFlowyBoardConfig(
-    groupBackgroundColor: HexColor.fromHex('#F7F8FC'),
+  final config = const AppFlowyBoardConfig(
+    groupBackgroundColor: Color(0xffF7F8FC),
     stretchGroupHeight: false,
   );
 
@@ -34,91 +40,123 @@ class _TicketListPageState extends State<TicketListPage> {
     super.initState();
 
     controller.enableGroupDragging(false);
-
-    for (var e in ticketStatus) {
-      controller.addGroup(AppFlowyGroupData(
-        id: e,
-        name: e.toLowerCase().replaceAll("_", " ").capitalized,
-        items: List.generate(10, (index) => TextItem("${index}_$e")),
-      ));
-
-      controller.getGroupController(e)!.enableDragging(false);
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(250, 250, 250, 1),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text('Ticket'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              await showDateRangePicker(
-                context: context,
-                initialDateRange: DateTimeRange(
-                  start: start,
-                  end: end,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => bloc),
+      ],
+      child: BlocListener<TicketListCubit, TicketListState>(
+        listener: (context, state) {
+          state.maybeWhen(
+            orElse: () {},
+            success: (success) {
+              Map<String?, List<TicketModel>> grouped = success.groupBy((e) => e.status);
+
+              for (var e in ticketStatus) {
+                controller.addGroup(AppFlowyGroupData(
+                  id: e,
+                  name: e.toLowerCase().replaceAll("_", " ").capitalized,
+                  items: (grouped[e] ?? []).map((e) => TextItem("${e.id}", e)).toList(),
+                ));
+
+                controller.getGroupController(e)!.enableDragging(false);
+              }
+            },
+          );
+        },
+        child: Scaffold(
+          backgroundColor: const Color.fromRGBO(250, 250, 250, 1),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: const Text('Ticket'),
+            actions: [
+              BlocBuilder<TicketListCubit, TicketListState>(
+                builder: (context, state) {
+                  return state.maybeMap(
+                    orElse: () => Container(),
+                    success: (success) => IconButton(
+                      onPressed: () async {
+                        await showDateRangePicker(
+                          context: context,
+                          initialDateRange: DateTimeRange(
+                            start: start,
+                            end: end,
+                          ),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                      },
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          body: BlocBuilder<TicketListCubit, TicketListState>(
+            builder: (context, state) {
+              return state.maybeMap(
+                orElse: () => const Center(child: CircularProgressIndicator.adaptive()),
+                failure: (failure) => Center(child: Text(failure.message)),
+                success: (success) => AppFlowyBoard(
+                  groupConstraints: const BoxConstraints(minWidth: 290, maxWidth: 300),
+                  config: const AppFlowyBoardConfig(
+                    groupPadding: EdgeInsets.zero,
+                    groupItemPadding: EdgeInsets.only(right: 8),
+                    headerPadding: EdgeInsets.symmetric(horizontal: 8),
+                    cardPadding: EdgeInsets.symmetric(vertical: 8),
+                    cornerRadius: 4,
+                  ),
+                  controller: controller,
+                  boardScrollController: boardScroll,
+                  cardBuilder: (BuildContext context, AppFlowyGroupData<dynamic> groupData, AppFlowyGroupItem item) {
+                    return AppFlowyGroupCard(
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      key: ValueKey(item.id),
+                      child: InkWell(
+                        onTap: () {
+                          context.router.push(TicketDetailRoute(ticketId: 1));
+                        },
+                        child: TicketCard(groupData: groupData),
+                      ),
+                    );
+                  },
+                  headerBuilder: (context, columnData) {
+                    return AppFlowyGroupHeader(
+                      margin: const EdgeInsets.only(top: 8),
+                      title: Row(
+                        children: [
+                          const SizedBox(width: 8),
+                          Text(
+                            columnData.headerData.groupName,
+                            style: CustomTextTheme.paragraph3.copyWith(fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(width: 8),
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            decoration: BoxDecoration(
+                              color: ColorTheme.primary.withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Text(
+                              '10',
+                              style: CustomTextTheme.paragraph1.copyWith(color: ColorTheme.primary),
+                            ),
+                          )
+                        ],
+                      ),
+                      height: 42,
+                    );
+                  },
                 ),
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
               );
             },
-            icon: const Icon(Icons.calendar_month),
           ),
-        ],
-      ),
-      body: AppFlowyBoard(
-        groupConstraints: const BoxConstraints(minWidth: 290, maxWidth: 300),
-        config: const AppFlowyBoardConfig(
-          groupPadding: EdgeInsets.zero,
-          groupItemPadding: EdgeInsets.only(right: 8),
-          headerPadding: EdgeInsets.symmetric(horizontal: 8),
-          cardPadding: EdgeInsets.symmetric(vertical: 8),
-          cornerRadius: 4,
         ),
-        controller: controller,
-        boardScrollController: boardScroll,
-        cardBuilder: (BuildContext context, AppFlowyGroupData<dynamic> groupData, AppFlowyGroupItem item) {
-          return AppFlowyGroupCard(
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            key: ValueKey(item.id),
-            child: InkWell(
-              onTap: () {
-                context.router.push(TicketDetailRoute(ticketId: 1));
-              },
-              child: TicketCard(groupData: groupData),
-            ),
-          );
-        },
-        headerBuilder: (context, columnData) {
-          return AppFlowyGroupHeader(
-            margin: const EdgeInsets.only(top: 8),
-            title: Row(
-              children: [
-                const SizedBox(width: 8),
-                Text(columnData.headerData.groupName,
-                    style: CustomTextTheme.paragraph3.copyWith(fontWeight: FontWeight.w600)),
-                const SizedBox(width: 8),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    color: ColorTheme.primary.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-                  child: Text(
-                    '10',
-                    style: CustomTextTheme.paragraph1.copyWith(color: ColorTheme.primary),
-                  ),
-                )
-              ],
-            ),
-            height: 42,
-          );
-        },
       ),
     );
   }
--- lib/presentation/pages/ticket/list/widgets/text_item.dart
+++ lib/presentation/pages/ticket/list/widgets/text_item.dart
@@ -1,79 +1,15 @@
 import 'package:appflowy_board/appflowy_board.dart';
-import 'package:flutter/material.dart';
+import 'package:next_starter/data/models/ticket/ticket_model.dart';
 
 class TextItem extends AppFlowyGroupItem {
   final String s;
+  final TicketModel model;
 
-  TextItem(this.s);
+  TextItem(this.s, this.model);
 
   @override
   IsDraggable get draggable => false;
 
   @override
   String get id => s;
-}
-
-class RichTextItem extends AppFlowyGroupItem {
-  final String title;
-  final String subtitle;
-
-  RichTextItem({required this.title, required this.subtitle});
-
-  @override
-  IsDraggable get draggable => false;
-
-  @override
-  String get id => title;
-}
-
-extension HexColor on Color {
-  static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
-  }
-}
-
-class RichTextCard extends StatefulWidget {
-  final RichTextItem item;
-  const RichTextCard({
-    required this.item,
-    super.key,
-  });
-
-  @override
-  State<RichTextCard> createState() => _RichTextCardState();
-}
-
-class _RichTextCardState extends State<RichTextCard> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.item.title,
-              style: const TextStyle(fontSize: 14),
-              textAlign: TextAlign.left,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              widget.item.subtitle,
-              style: const TextStyle(fontSize: 12, color: Colors.grey),
-            )
-          ],
-        ),
-      ),
-    );
-  }
 }
