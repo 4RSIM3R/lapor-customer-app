6eb8448035765fdb57050f1bec18bf6cf380189a
ilzam <ilzammulkhaq85@gmail.com>
2024-03-31 22:46:15 +0700

    feat: get detail ticket

--- lib/presentation/pages/ticket/detail/ticket_detail_page.dart
+++ lib/presentation/pages/ticket/detail/ticket_detail_page.dart
@@ -1,9 +1,10 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:casa_vertical_stepper/casa_vertical_stepper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:next_starter/injection.dart';
-import 'package:next_starter/presentation/components/button/primary_button.dart';
 import 'package:next_starter/presentation/pages/ticket/detail/cubit/ticket_detail_cubit.dart';
+import 'package:next_starter/presentation/pages/ticket/detail/widgets/ticket_detail_card.dart';
 import 'package:next_starter/presentation/theme/theme.dart';
 
 @RoutePage()
@@ -21,164 +22,58 @@ class _TicketDetailPageState extends State<TicketDetailPage> {
 
   final bloc = locator<TicketDetailCubit>();
 
-  final stepperList = [
-    StepperStep(
-      title: Text(
-        '13 March 2024',
-        style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600),
-      ),
-      leading: const Icon(Icons.circle, color: ColorTheme.primary, size: 18),
-      isExpanded: true,
-      visible: true,
-      status: StepStatus.inprogress,
-      view: const Column(
-        children: [Text('Status')],
-      ),
-    ),
-    StepperStep(
-      title: const Text('Account Details'),
-      leading: const Icon(Icons.circle),
-      view: Container(),
-    ),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    bloc.get(id: widget.ticketId);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => bloc),
+      ],
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.white,
           title: const Text('Ticket Information'),
         ),
-        body: Theme(
-          data: ThemeData(
-            colorScheme: Theme.of(context).colorScheme.copyWith(primary: ColorTheme.primary),
-          ),
-          child: Stepper(
-            type: StepperType.vertical,
-            currentStep: currentStep,
-            margin: const EdgeInsetsDirectional.only(start: 60.0),
-            controlsBuilder: (context, details) => Container(),
-            onStepTapped: (index) {
-              setState(() => currentStep = index);
-            },
-            steps: [
-              Step(
-                title: Text('13 March 2024', style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600)),
-                isActive: true,
-                state: StepState.indexed,
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text('Action', style: CustomTextTheme.paragraph1),
-                    const SizedBox(height: 4),
-                    Text(
-                      'Fixing Broken Pipe In Sector',
-                      style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(height: 8),
-                    Text('Employee', style: CustomTextTheme.paragraph1),
-                    const SizedBox(height: 4),
-                    Text('MR John Doe', style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600)),
-                    const SizedBox(height: 8),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-                      style: CustomTextTheme.paragraph1,
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 90,
-                          child: PrimaryButton(
-                            title: 'See Photos',
-                            isSmall: true,
-                            isFullWidth: false,
-                            onTap: () {},
+        body: BlocBuilder<TicketDetailCubit, TicketDetailState>(
+          builder: (context, state) {
+            return state.maybeMap(
+              orElse: () => const Center(child: CircularProgressIndicator.adaptive()),
+              failure: (failure) => Center(child: Text(failure.message)),
+              success: (success) => Theme(
+                data: ThemeData(
+                  colorScheme: Theme.of(context).colorScheme.copyWith(primary: ColorTheme.primary),
+                ),
+                child: Stepper(
+                  type: StepperType.vertical,
+                  currentStep: currentStep,
+                  margin: const EdgeInsetsDirectional.only(start: 60.0),
+                  controlsBuilder: (context, details) => Container(),
+                  onStepTapped: (index) {
+                    setState(() => currentStep = index);
+                  },
+                  steps: (success.payload.histories ?? [])
+                      .map(
+                        (e) => Step(
+                          title: Text(
+                            DateFormat('dd MMMMM yyyy').format(e.createdAt!),
+                            style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600),
                           ),
+                          isActive: true,
+                          state: StepState.indexed,
+                          content: TicketDetailCard(history: e),
                         ),
-                      ],
-                    )
-                  ],
+                      )
+                      .toList(),
                 ),
               ),
-              Step(
-                title: const Text('17 March 2024'),
-                isActive: true,
-                state: StepState.indexed,
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text('Action', style: CustomTextTheme.paragraph1),
-                    const SizedBox(height: 4),
-                    Text('Fixing Broken Pipe In Sector',
-                        style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600)),
-                    const SizedBox(height: 8),
-                    Text('Employee', style: CustomTextTheme.paragraph1),
-                    const SizedBox(height: 4),
-                    Text('MR John Doe', style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600)),
-                    const SizedBox(height: 8),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-                      style: CustomTextTheme.paragraph1,
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 90,
-                          child: PrimaryButton(
-                            title: 'See Photos',
-                            isSmall: true,
-                            isFullWidth: false,
-                            onTap: () {},
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Step(
-                title: const Text('Done'),
-                isActive: true,
-                state: StepState.complete,
-                content: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text('Action', style: CustomTextTheme.paragraph1),
-                    const SizedBox(height: 4),
-                    Text('Fixing Broken Pipe In Sector',
-                        style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600)),
-                    const SizedBox(height: 8),
-                    Text('Employee', style: CustomTextTheme.paragraph1),
-                    const SizedBox(height: 4),
-                    Text('MR John Doe', style: CustomTextTheme.paragraph2.copyWith(fontWeight: FontWeight.w600)),
-                    const SizedBox(height: 8),
-                    Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-                      style: CustomTextTheme.paragraph1,
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 90,
-                          child: PrimaryButton(
-                            title: 'See Photos',
-                            isSmall: true,
-                            isFullWidth: false,
-                            onTap: () {},
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
